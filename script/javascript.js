1885f53c329498ab3feb5195e848493068813507
Ayesha8888 <ayeshaajuii248@gmail.com>
2024-02-21 13:28:04 +0600

    update

--- script/javascript.js
+++ script/javascript.js
@@ -26,37 +26,4 @@ for (const seat of seatsAll) {
   });
 }
 
-// const allBtn = document.getElementsByClassName("add-btn");
-// let count = 0;
-// let totalPrice = 0;
-// let seatsLeft = 36;
-
-// for (const btn of allBtn) {
-//   btn.addEventListener("click", function (e) {
-//     count = count + 1;
-//     document.getElementById("seat-count").innerText = count;
-
-//     const seatPrice = 550;
-//     totalPrice += seatPrice;
-//     document.getElementById("total-price").innerText = totalPrice;
-
-//     seatsLeft -= 1;
-//     document.getElementById("seats-left").innerText = seatsLeft;
-
-//     applyDiscount();
-//   });
-// }
-
-// function applyDiscount() {
-//   const couponInput = document.getElementById("coupon-code").value;
-//   let discountPercentage = 0;
-
-//   if (couponInput === "NEW15") {
-//     discountPercentage = 15;
-//   } else if (couponInput === "Couple20") {
-//     discountPercentage = 20;
-//   }
-
-//   const discountedPrice = totalPrice - (totalPrice * discountPercentage) / 100;
-//   document.getElementById("discounted-price").innerText = discountedPrice;
-// }
+countedPrice = totalPrice - (totalPrice * discountPercentage) / 100;
