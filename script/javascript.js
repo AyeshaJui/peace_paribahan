a3d3e26885c7816629c0b9ebb54cdfbca1a58ac7
Ayesha8888 <ayeshaajuii248@gmail.com>
2024-02-22 03:35:14 +0600

    functional work

--- script/javascript.js
+++ script/javascript.js
@@ -1,29 +1,66 @@
-const allBtn = document.getElementsByClassName("add-btn");
-let count = 0;
+const allSeat = document.getElementsByClassName("add-btn");
 
-for (const btn of allBtn) {
-  btn.addEventListener("click", function (e) {
-    count = count + 1;
-    document.getElementById("seat-count").innerText = count;
+for (const seat of allSeat) {
+  seat.addEventListener("click", function (event) {
+    const name = event.target.parentNode.childNodes[1].innerText;
 
-    const seatTicket = e.target.parentNode.childNode[1].innerText;
+    const economy = event.target.parentNode.childNodes[3].innerText;
 
-    setInnerTex("seat-count", count);
+    const price = event.target.parentNode.childNodes[5].innerText;
+
+    console.log(event.target.parentNode.childNodes);
+
+    const selectedContainer = document.getElementById(
+      "selected-seat-container"
+    );
+
+    const div = document.createElement("div");
+    div.classList.add("selected-seats");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    p1.innerText = name;
+    p2.innerText = economy;
+    p3.innerText = price;
+
+    div.appendChild(p1);
+    div.appendChild(p2);
+    div.appendChild(p3);
+
+    selectedContainer.appendChild(div);
+    totalCost(price);
+    updateGrandTotal();
   });
 }
 
-function setInnerTex(id, value) {
-  document.getElementById(id).innerText = value;
+function updateGrandTotal(update) {
+  const totalPrice = getConvertedValue("total-price");
+  if (update == undefined) {
+    document.getElementById("grand-total").innerText = totalPrice;
+  } else {
+    const couponCode = document.getElementById("coupon-code").value;
+    if (couponCode == "NEW15") {
+      const discountedPrice = totalPrice * 0.15;
+      document.getElementById("grand-total").innerText =
+        totalPrice - discountedPrice;
+    } else if (couponCode == "Couple 20") {
+      const discountedPrice = totalPrice * 0.2;
+      document.getElementById("grand-total").innerText =
+        totalPrice - discountedPrice;
+    } else {
+      alert("Please enter valid coupon code");
+    }
+  }
 }
 
-for (const seat of seatsAll) {
-  seat.addEventListener("click", function () {
-    const selectSeats = document.getElementById("title-seats");
-    const li = document.createElement("li");
-    const seatsDetails = allSeat.children[0].children[1].children.innerText;
-    li.innerText = seatsDetails;
-    selectSeats.appendChild(li);
-  });
+function totalCost(value) {
+  const totalPrice = getConvertedValue("total-price");
+  const sum = totalPrice + parseInt(value);
+  document.getElementById("total-price").innerText = sum;
 }
 
-countedPrice = totalPrice - (totalPrice * discountPercentage) / 100;
+function getConvertedValue(id) {
+  const price = document.getElementById(id).innerText;
+  const convertPrice = parseInt(price);
+  return convertPrice;
+}
